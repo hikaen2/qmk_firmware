0f9ef09c6aca8b27713398d5c53f2d52921e55eb
Suzuki Taro <4222502+hikaen2@users.noreply.github.com>
2023-12-01 18:46:38 +0900

    wip

--- keyboards/planck/keymaps/hikaen2/keymap.c
+++ keyboards/planck/keymaps/hikaen2/keymap.c
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
- * | Esc  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   (  |   )  |      |  `   |
+ * | Esc  |   !  |   "  |   #  |   $  |   %  |   &  |   '  |   (  |   )  |      |  `   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   {  |   }  |   +  |  |   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_planck_grid(
-    KC_ESC,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, JP_GRV,
+    KC_ESC,  JP_EXLM, JP_DQUO, JP_HASH, JP_DLR,  JP_PERC, JP_AMPR, JP_QUOT,    JP_LPRN,    JP_RPRN, KC_RPRN, JP_GRV,
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    JP_LCBR,    JP_RCBR, KC_PLUS, KC_PIPE,
     _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,  _______,
     _______, _______, _______, _______, _______, _______, _______, _______,    _______,    _______, _______, _______
